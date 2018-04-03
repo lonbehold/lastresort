da3372cabd0b5a504f3b7e792cc2faedcb6d4e23
John Wu <lonbehold@github.com>
2018-04-03 13:52:05 -0700

    minor change

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -384,11 +384,11 @@ resource "aws_elb" "lbforweb" {
   }
 
   health_check {
-    healthy_threshold = 10
+    healthy_threshold = 2
     unhealthy_threshold = 2
-    timeout = 2
+    timeout = 5
     target = "HTTP:80/"
-    interval = 10
+    interval = 30
   }
 
   instances = ["${aws_instance.weba.id}", "${aws_instance.webb.id}"]
