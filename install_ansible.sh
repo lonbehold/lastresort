337f43570c56bdbf235cadaef4a61235dc32b09b
unknown <johnwu007@hotmail.com>
2018-04-27 20:55:10 -0700

    added more stuff to install

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,4 +3,7 @@ sudo apt-get update  # To get the latest package lists
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
+sudo apt-get install unzip -y
+sudo apt-get install mysql-server -y
+sudo apt-get install python-mysqldb -y
 #etc.
